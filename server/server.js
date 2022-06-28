be87741d95e124d769e153d8cbd26fae8e7fdc7a
okoik <ott.koik@gmail.com>
2022-06-28 22:14:31 +0300

    add database connection

--- server/server.js
+++ server/server.js
@@ -1,19 +1,32 @@
 const express = require('express')
 const cors = require("cors");
 const auth = require("./routes/auth");
+const tournaments = require("./routes/tournaments");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongo = require("mongoose");
+
+const db_uri = "mongodb+srv://appuser:gaZsPDffU2MnFDpu0B3q@sports-prediction-dev.tpeof.mongodb.net/sports_prediction?retryWrites=true&w=majority";
+global.db = (global.db ? global.db : mongo.createConnection(db_uri));
 
 const app = express();
-
 
 app.use(express.json());
 app.use(cors());
 app.use("/auth", auth);
+app.use("/tournaments", tournaments);
 
+app.use(function (req, res, next){
+  res.setHeader('Access-Control-Allow-Origin','http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers','X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials',true);
+next();
+})
 
 app.get("/", (req, res) => {
   res.send("lol route working")
 })
-
 
 app.listen(8080, () => {
   console.log("Running on port 8080");
