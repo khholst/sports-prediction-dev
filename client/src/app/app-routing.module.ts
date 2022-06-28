4bf0d1ccfa0ef50d9a8cdfe2f83a14ea50610315
okoik <ott.koik@gmail.com>
2022-06-28 22:12:38 +0300

    add tournaments URL

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { TournamentsComponent } from './tournaments/tournaments.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'tournaments', component: TournamentsComponent}
 ];
 
 @NgModule({
