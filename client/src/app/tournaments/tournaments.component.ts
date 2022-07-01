5585fa89327e6145e7c0535dcaea2a65424496bf
okoik <ott.koik@gmail.com>
2022-07-01 23:55:56 +0300

    Fixed date

--- client/src/app/tournaments/tournaments.component.ts
+++ client/src/app/tournaments/tournaments.component.ts
@@ -60,8 +60,8 @@ export class TournamentsComponent implements OnInit {
     const monthLookup: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
                                   "October", "November", "December"];
     const date = new Date(dateString);
-    if(!time){return `${date.getDate()} ${monthLookup[date.getMonth()]}`}
-    else{return`${date.getDate()} ${monthLookup[date.getMonth()]} ${date.getHours().toString().replace(/^0$/,"00")}:${date.getMinutes().toString().replace(/^0$/,"00")}`};
+    if(!time){return `${date.getDate()}. ${monthLookup[date.getMonth()]}`}
+    else{return`${date.getDate()}. ${monthLookup[date.getMonth()]} ${date.getHours().toString().replace(/^0$/,"00")}:${date.getMinutes().toString().replace(/^0$/,"00")}`};
   };
 
 }
