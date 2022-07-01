7fc8664e5dc61ec5530f5d4397c0a883d15a6b52
khholst <karlhendrikholst@gmail.com>
2022-07-01 14:05:22 +0300

    Worked on tournaments page

---
+++ client/src/app/tournament.ts
@@ -0,0 +1,9 @@
+export class Tournament {
+    constructor(
+        public name:string,
+        public tournament_id:number,
+        public start_date:string,
+        public end_date:string,
+        public _id:string
+    ) {}
+}
--- client/src/app/tournaments/tournaments.component.ts
+++ client/src/app/tournaments/tournaments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
+import { Tournament } from '../tournament';
 
 
 @Component({
@@ -9,6 +10,9 @@ import { DataService } from '../data.service';
   styleUrls: ['./tournaments.component.css']
 })
 export class TournamentsComponent implements OnInit {
+  public isCollapsed: boolean[] = [];
+  public tournaments: Tournament[] = [];
+  public indexes: number[] = []; //For keeping track of expanded tournaments
 
   constructor(
     private dataService: DataService,
@@ -17,11 +21,29 @@ export class TournamentsComponent implements OnInit {
 
   ngOnInit(): void {
     this.onRequest();
+
   };
 
   async onRequest() {
-    const response = await this.dataService.getTournaments();
-    console.log(response);
+    this.tournaments = await this.dataService.getTournaments();
+
+    let index = 0; //For keeping track of expanded tournaments
+    for (let i = 0; i < this.tournaments.length; i++) {
+      this.indexes.push(index);
+      this.isCollapsed.push(true);
+      this.tournaments[i].start_date = this.formatDate(this.tournaments[i].start_date)
+      this.tournaments[i].end_date = this.formatDate(this.tournaments[i].end_date)
+      console.log(this.tournaments)
+      index++;
+    }
   };
+
+  private formatDate(dateString: string): string {
+    const monthLookup: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+                                  "October", "November", "December"];
+    const date = new Date(dateString); 
+
+    return `${date.getDate()} ${monthLookup[date.getMonth()]}`;
+  }
 
 }
