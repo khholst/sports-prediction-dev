432648a83508e2d8febec04c02bdfe56d45cfd70
okoik <ott.koik@gmail.com>
2022-07-07 22:23:06 +0300

    Started with room component

--- client/src/app/room/room.component.ts
+++ client/src/app/room/room.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../user';
+import { RoomService } from '../room.service';
 
 @Component({
   selector: 'app-room',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private roomService: RoomService
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.onRoomRequest();
+  };
+
+  async onRoomRequest(){
+    let roomID: string = this.route.snapshot.paramMap.get("id")!;
+    let roomUsers: Array<User> = await this.roomService.getRoomUsers([roomID]);
+    console.log(roomUsers)
+  };
 
 }
