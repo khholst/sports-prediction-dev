30363234253cf46d3b77d80176ab6577aec22b84
okoik <ott.koik@gmail.com>
2022-07-13 22:02:51 +0300

    Worked with room page

--- client/src/app/room.ts
+++ client/src/app/room.ts
@@ -1,17 +1,17 @@
 export class Room {
     constructor(
-        public tournament_id: string,
-        public name: string,
-        public creator: string,
-        public join_key: string,
-        public _id: string,
-        public tournament: string,
-        public start_date: string,
-        public end_date: string,
-        public status: string,
-        public numUsers: number,
-        public timeUntil: number,
-        public userPos: number,
-        public leader: string
+        public tournament_id: string = "",
+        public name: string = "",
+        public creator: string = "",
+        public join_key: string = "",
+        public _id: string = "",
+        public tournament: string = "",
+        public start_date: string = "",
+        public end_date: string = "",
+        public status: string = "",
+        public numUsers: number = 0,
+        public timeUntil: number = 0,
+        public userPos: number = 0,
+        public leader: string = ""
     ) {}
 }
--- client/src/app/room/room.component.ts
+++ client/src/app/room/room.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '../user';
 import { Room } from '../room';
+import { Tournament } from '../tournament';
 import { RoomService } from '../room.service';
+import { DataService } from '../data.service';
+import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-room',
@@ -12,10 +15,14 @@ import { RoomService } from '../room.service';
 export class RoomComponent implements OnInit {
   public roomUsers: User[] = [];
   public roomName: string = "";
+  public room = new Room();
+  public faTrophy = faTrophy;
+  public active: number = 1;
 
   constructor(
     private route: ActivatedRoute,
-    private roomService: RoomService
+    private roomService: RoomService,
+    private dataService: DataService
   ) { }
 
   ngOnInit(): void {
@@ -24,13 +31,43 @@ export class RoomComponent implements OnInit {
 
   async onRoomRequest(){
     const roomID: string = this.route.snapshot.paramMap.get("id")!;
-    const room = await this.roomService.getRoom(roomID);
-    this.roomName = room[0].name
+    const rooms: Array<Room> = await this.roomService.getRoom(roomID);
+    this.room = rooms[0];
+    
+    const tournaments: Array<Tournament> = await this.dataService.getTournaments(this.room.tournament_id);
+    this.room.tournament = tournaments[0].name;
+    this.room.start_date = tournaments[0].start_date;
+    this.room.end_date = tournaments[0].end_date;
+
+    const now = new Date();
+    const start_date = new Date(this.room.start_date);
+    const end_date = new Date(this.room.end_date);
+
+
+    if (now < start_date) {
+      this.room.status = "W";
+      let diffInMs: number = Math.abs(start_date.valueOf() - now.valueOf());
+      let diff: number =  diffInMs / (1000 * 60 * 60 * 24);
+      this.room.timeUntil = Math.ceil(diff);
+    } else if (now < end_date) {
+        this.room.status = "A";
+    } else {
+        this.room.status = "E";
+    };
+    
+
     this.roomUsers = await this.roomService.getRoomUsers([roomID]);
     this.roomUsers.sort(
       (firsUser: User, secondUser: User) =>
         (firsUser.rooms[0].score > secondUser.rooms[0].score) ? -1 : 1
     );
+  };
+
+  formatDate(dateString: string): string {
+    const monthLookup: string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+                                  "October", "November", "December"];
+    const date = new Date(dateString);
+    return `${date.getDate()}. ${monthLookup[date.getMonth()]}`
   };
 
 }
